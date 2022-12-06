564d8b05c0da4ccec6bd72d62e2cccac4c558ac1
Chris <Chrisscheer211@gmail.com>
2022-12-05 23:21:56 -0500

    Calendar Functionality

---
+++ app/src/main/java/com/example/RainCheck/CalanderActivity.kt
@@ -0,0 +1,64 @@
+/**
+ * The main project package.
+ */
+package com.example.raincheck
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CalendarView
+import com.example.raincheck.databinding.ActivityCalanderBinding
+
+class CalanderActivity : AppCompatActivity() {
+
+    /**
+     * Private Attributes.
+     */
+    private lateinit var binding: ActivityCalanderBinding
+    private lateinit var calander: CalendarHelper
+
+    private lateinit var dateString: String
+
+    /**
+     *
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        /**
+         *
+         */
+        super.onCreate(savedInstanceState)
+        this.binding = ActivityCalanderBinding.inflate(layoutInflater)
+
+        setContentView(this.binding.root)
+
+        /**
+         * Sets a text view with the date the users selects on the calendar.
+         */
+        this.binding.calanderCalanderView.setOnDateChangeListener(
+            CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
+
+                /**
+                 * Converts the date to the string that is used later.
+                 */
+                this.dateString = (year.toString() + "-" + (month + 1) + "-" + dayOfMonth.toString())
+                this.binding.dateTextTextView.setText(dateString)
+            }
+        )
+
+        /**
+         *
+         */
+        this.binding.button.setOnClickListener {
+
+            /**
+             *
+             */
+            this.calander = CalendarHelper()
+            this.calander.setDateWithString(this.dateString + "T09:00:00", this.dateString + "T12:00:00")
+            //this.calander.setDateWithString("2022-02-1T09:00:00", "2022-02-1T12:00:00")
+
+            startActivity(this.calander.createCalendarEvent("RainCheck Appointment"))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/RainCheck/CalendarHelper.kt
@@ -0,0 +1,86 @@
+package com.example.raincheck
+
+import android.content.Intent
+import android.util.Log
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CalendarHelper {
+
+
+    /**
+     * Private Attributes.
+     */
+    private lateinit var sTime: Date    //Start time.
+    private lateinit var eTime: Date    //End time.
+
+    /**
+     * Set the date with a string representation.
+     */
+    public fun setDateWithString(sDate: String, eDate:String): Boolean{
+
+        /**
+         *
+         */
+        if(sDate.equals(null)) return false
+        if(eDate.equals(null)) return false
+
+        /**
+         * Test the follows block of code to NullPointerException or IllegalArgumentException.
+         */
+        try {
+
+            val mSimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            this.sTime = mSimpleDateFormat.parse(sDate) as Date
+            this.eTime = mSimpleDateFormat.parse(eDate) as Date
+        }
+
+        /**
+         *
+         */
+        catch (e: java.lang.Exception){
+
+            Log.e("Calander", e.toString())
+            return false
+        }
+
+        return true
+    }
+
+    /**
+     * Set calendar event.
+     */
+    public fun createCalendarEvent(title: String): Intent {
+
+        /**
+         *
+         */
+        val mIntent: Intent = Intent(Intent.ACTION_EDIT)
+
+        /**
+         *
+         */
+        if(title.isNotEmpty()){
+
+            mIntent.type = "vnd.android.cursor.item/event"
+            mIntent.putExtra("beginTime", this.sTime.time)
+            mIntent.putExtra("time", true)
+            mIntent.putExtra("rule", "FREQ=YEARLY")
+            mIntent.putExtra("endTime", this.eTime.time)
+            mIntent.putExtra("title",  title)
+        }
+
+        return mIntent
+    }
+
+    /**
+     * Sets the date to be set in the calendar.
+     */
+    private fun createCalanderEvent(
+        sYear: Short, sDay : Short, sMonth : Short, sHour: Byte, sMin: Byte,
+        eYear: Short, eDay : Short, eMonth : Short, eHour: Byte, eMin: Byte
+    ) : Boolean{
+
+        return true
+    }
+}
